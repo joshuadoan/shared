df6e1d350acfa487be5fc6e67d2f3f4728101673
Joshua <insaddingaround@gmail.com>
2024-07-18 11:08:34 -0700

    Initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,18 @@
+<script  lang="ts">
+export default {
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<template>
+<div class="hello-world">Shared  {{ msg  }}</div>
+</template>
+
+<style scoped  lang="scss">
+$primary: #ff9900;
+.hello-world {
+  background: $primary;
+}
+</style>
